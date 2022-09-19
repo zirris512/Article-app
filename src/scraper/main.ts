97115e376a1be4c5592c9a57dcfb0db17e411427
zirris512 <zirris512@gmail.com>
2022-09-19 00:09:48 -0500

    add scraped data needed to send to db

--- src/scraper/main.ts
+++ src/scraper/main.ts
@@ -1,14 +1,21 @@
 import puppeteer from "puppeteer";
 
 export const scraper = async () => {
-    const browser = await puppeteer.launch({
-        headless: false,
-    });
+    const browser = await puppeteer.launch();
     const page = await browser.newPage();
     await page.goto("https://blog.logrocket.com");
-    await page.waitForSelector(".featured-posts .card");
+    await page.waitForSelector(".featured-posts");
     const data = await page.$$eval(".featured-posts .card", (els) => {
-        return els.map((el) => el.querySelector(".card-title a")?.textContent);
+        return els.map((el) => {
+            const title = el.querySelector(".card-title a")?.textContent;
+            const description = el.querySelector(".card-text")?.textContent;
+            const link = el.querySelector<HTMLAnchorElement>(".card-title a")?.href;
+            return {
+                title,
+                description,
+                link,
+            };
+        });
     });
     return data;
 };
